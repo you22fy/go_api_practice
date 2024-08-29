a6725fa64441e4d7eaef48512195044324be19aa
Yuki Fujita <uni22yu@gmail.com>
2024-08-30 01:04:20 +0900

    feat : 1-3

--- handlers/handler.go
+++ handlers/handler.go
@@ -8,32 +8,56 @@ import (
 
 // helloのハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, World!\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello, World!\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /articleのハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "posting article...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "posting article...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /article/listのハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /article/1のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleId := 1
-	resString := fmt.Sprintf("Article No.%d\n", articleId)
-	io.WriteString(w, resString)
+	if req.Method == http.MethodGet {
+		articleId := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleId)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /article/nice のハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /comment のハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Comment...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
